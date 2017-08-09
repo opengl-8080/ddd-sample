5f27f13f15003c3da5c28d46a354f380c083c02c
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-08-09 23:16:58 +0900

    Thymeleaf のレイアウトの仕組みを導入

--- transaction-script/src/main/java/ts/web/IndexController.java
+++ transaction-script/src/main/java/ts/web/IndexController.java
@@ -1,16 +1,13 @@
 package ts.web;
 
-import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
     
-    @GetMapping({"/index", "/"})
-    public String index(Model model, CsrfToken csrfToken) {
-        model.addAttribute("csrf", csrfToken);
+    @GetMapping("/")
+    public String index() {
         return "index";
     }
 }
--- transaction-script/src/main/java/ts/web/LoginController.java
+++ transaction-script/src/main/java/ts/web/LoginController.java
@@ -1,16 +1,13 @@
 package ts.web;
 
-import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
     
     @GetMapping("/login")
-    public String login(Model model, CsrfToken csrfToken) {
-        model.addAttribute("csrf", csrfToken);
+    public String login() {
         return "login";
     }
 }
